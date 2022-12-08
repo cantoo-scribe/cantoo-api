db460eb0d7af3d44ff864eefa467604f4d1bb17d
François Billioud <f.billioud@gmail.com>
2022-12-08 03:41:38 +0100

    Fix release process

--- release.sh
+++ release.sh
@@ -3,16 +3,25 @@
 set -e
 
 node ./updateVersion.js
-version=awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json
+echo "Version updated in package.json"
+version=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json`
 
-read -p "Release version $version? " -n 1 -r
+read -p "Release version $version? (y/N)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   git add .
   git commit -m "Version $version"
+  echo "Commit: 'Version $version' created"
   npm publish
+  echo "Published in npm"
   git tag "v$version"
+  echo "Tag created"
   git push origin main
+  echo "Commit pushed to main"
   git push origin --tag
+  echo "Tag pushed on the repository"
+else
+  node ./updateVersion.js -revert
+  echo "package.json reverted"
 fi
--- updateVersion.js
+++ updateVersion.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
-const package = fs.readFileSync('./package.json')
+const package = fs.readFileSync('./package.json', 'utf8')
 const version = package.match(/"version": "(\d+\.\d+\.\d+)"/)[1]
 const [major, minor, patch] = version.split('.')
-const newVersion = `${major}.${minor}.${parseInt(patch) + 1}`
-fs.writeFileSync('./package.json', package.replace(`"version": "${version}"`, `"version": "${newVersion}`))
+const increment = (process.argv[2] === '-revert') ? -1 : 1
+const newVersion = `${major}.${minor}.${parseInt(patch) + increment}`
+fs.writeFileSync('./package.json', package.replace(`"version": "${version}"`, `"version": "${newVersion}"`), 'utf8')
